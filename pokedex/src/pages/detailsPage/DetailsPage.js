e99af08872ecf7edc95f79b012a3b53914ee3da2
joaopedrocoder <joaopedrolr.7@gmail.com>
2021-07-12 15:40:55 -0300

    criaçao do router

---
+++ pokedex/src/pages/detailsPage/DetailsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DetailsPage = () => {
+    return (
+        <div>
+            <h1>Detalhes</h1>
+        </div>
+    )
+}
+
+export default DetailsPage
---
+++ pokedex/src/pages/pokedexPage/PokedexPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PokedexPage = () => {
+    return (
+        <div>
+            <h1>Pokedex</h1>
+        </div>
+    )
+}
+
+export default PokedexPage
