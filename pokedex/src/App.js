b2eb44952c8042876698d1088c424a4c23df6c5e
joaopedrocoder <joaopedrolr.7@gmail.com>
2021-07-12 16:13:30 -0300

    criacao do globalStyle

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
 import Router from "./route/Router";
+import styled, { createGlobalStyle} from 'styled-components'
+
+const GlobalStyle =  createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    background-color: #202020;
+  }
+`
 
 function App() {
   return (
     <div>
+      <GlobalStyle/>
+      
       <Router/>
     </div>
   );
