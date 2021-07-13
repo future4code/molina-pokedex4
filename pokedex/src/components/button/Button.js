5f0db0a0319942aec8bd527318d80eea584d0cad
artagnancoder <stayhomer@outlook.com>
2021-07-13 15:48:35 -0300

    não aceitar

--- pokedex/src/components/button/Button.js
+++ pokedex/src/components/button/Button.js
@@ -2,9 +2,14 @@ import React from 'react'
 import {Botão} from './styled'
 
 const Button = (props) => {
+
+    const irPara = () => {
+        console.log('certo')
+    }
+
     return (
         <div>
-            <Botão>{props.nome}</Botão>
+            <Botão onClick={irPara}>{props.nome}</Botão>
         </div>
     )
 }
--- pokedex/src/components/header/Header.js
+++ pokedex/src/components/header/Header.js
@@ -3,6 +3,9 @@ import {HeaderContainer, LogoPokemon} from './styled'
 import Botão from '../button/Button'
 
 const Header = () => {
+
+
+
     return (
         <HeaderContainer>
             <LogoPokemon src='https://i2.wp.com/multarte.com.br/wp-content/uploads/2019/03/pokemon-png-logo.png?fit=2000%2C736&ssl=1' alt='Logo-Pokemon' />  
