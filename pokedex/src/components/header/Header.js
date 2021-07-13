c40cc6899ccf827056c2811f637db17728e67d89
joaopedrocoder <joaopedrolr.7@gmail.com>
2021-07-13 16:10:19 -0300

    mais correcoes

--- pokedex/src/components/header/Header.js
+++ pokedex/src/components/header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <HeaderContainer>
             <LogoPokemon src='https://i2.wp.com/multarte.com.br/wp-content/uploads/2019/03/pokemon-png-logo.png?fit=2000%2C736&ssl=1' alt='Logo-Pokemon' />  
-            <Botão texto={btn.texto} onClick={mudaPagina}/>
+            <Botão texto='Ir para pkedex' />
         </HeaderContainer>
     )
 }
