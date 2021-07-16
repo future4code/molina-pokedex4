e85b9ef38a56967c27da5d3f8c276c86a9ba4bfa
artagnancoder <stayhomer@outlook.com>
2021-07-16 16:29:02 -0300

    Correção da função de adicionar pokemons a pokedex

--- pokedex/src/pages/homePage/HomePage.js
+++ pokedex/src/pages/homePage/HomePage.js
@@ -7,59 +7,24 @@ import Botão from '../../components/botao/Botao'
 import PokeCard from '../../components/pokecard/PokeCard'
 
 const HomePage = () => {
-
-
     const history = useHistory()
-
+    const pokemons = useContext(GlobalStateContext)
     const irParaPokedex = () => {
-
- 
         history.push('/pokedex')
     }
-
-
-    const addPokedex = (()=>{
-        alert('Adicionado')
-      
-    const pokemons = useContext(GlobalStateContext)
-
-    /* const pokemon = pokemons.pokemons
-    const pokedex = pokemons.pokedex
- */
-
-    console.log('POKEMONS', pokemons.pokemons)
-
-    console.log('POKEDEX', pokemons.pokedex)
-
-    /*  useEffect(()=>{
-         const pokemonsFiltrados = pokemons.pokemons.filter((pokemon) =>{
-             return pokemon.pokemons !== pokemon.pokedex
-             
-         })
-         
-         
-     }, [pokemons.pokedex]) */
-
-
     const addPokedex = ((pokemon) => {
         alert(`${pokemon.name} adicionado a pokedex!`)
         pokemons.setPokedex([...pokemons.pokedex, pokemon])
-        /* pokemons.setPokemons([...pokemons.pokemons, pokemons.pokemons.filter((pokemon) => {
-            return pokemon.pokemons !== pokemon.pokedex
-
-        }) ]) */
-
-        /* const indice = pokemons.pokemons.indexOf(pokemon.name) 
-        const pokemonRemovidoHome = pokemons.pokemons.splice(pokemon.id, 1)
-       
-       console.log('POKEMON REMOVIDO', pokemonRemovidoHome)  */
+        
 
 
     })
+    const detalhes = (pokeNome) => {
+        history.push(`/details/${pokeNome}`)
+        console.log(pokeNome)
+    }
 
-
-
-  /*   const pokemonsFiltrados = pokemons.pokemons.filter((pokemon) => {
+    const pokemonsFiltrados = pokemons.pokemons.filter((pokemon) => {
         const pokedex = pokemons.pokedex.find((pokemonEscolhido) => {
             if (pokemon.id === pokemonEscolhido.id) {
                 return true
@@ -73,16 +38,9 @@ const HomePage = () => {
             return true
         }
 
-    }) */
+    })
 
-
-
-    const detalhes = (pokeNome) =>{
-        history.push(`/details/${pokeNome}`)
-        console.log(pokeNome)
-    }
-
-
+    
     return (
         <div>
             <HeaderContainer>
@@ -91,11 +49,10 @@ const HomePage = () => {
                     <LogoPokemon src='https://i2.wp.com/multarte.com.br/wp-content/uploads/2019/03/pokemon-png-logo.png?fit=2000%2C736&ssl=1' alt='Logo-Pokemon' />
                 </DivContainer>
             </HeaderContainer>
-
             <ContainerConteudo>
                 <h1>Lista de Pokemons</h1>
                 <DivCards>
-                    {pokemons.pokemons.map((pokemon) => {
+                    {pokemonsFiltrados.map((pokemon) => {
                         return (
                             <PokeCard
                                 key={pokemon.id}
@@ -106,14 +63,10 @@ const HomePage = () => {
                                     return (
                                         <span key={index}> -{tipo.type.name}</span>
                                     )
-
                                 })}
-                                hp={pokemon.stats[0].base_stat}
-                                att={pokemon.stats[1].base_stat}
-                                def={pokemon.stats[2].base_stat}
                                 nomeBotaoEsquerdo="+ pokedex"
                                 onClickPokedex={() => addPokedex(pokemon)}
-
+                                onClickDetalhes={() => detalhes(pokemon.name)}
                             />
                         )
                     })}
@@ -123,5 +76,4 @@ const HomePage = () => {
         </div>
     )
 }
-
 export default HomePage
