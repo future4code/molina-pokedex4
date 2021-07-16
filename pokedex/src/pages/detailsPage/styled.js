783e270b2fd2f7e6ab96060969dfae5bf2e59076
joaopedrocoder <joaopedrolr.7@gmail.com>
2021-07-16 19:33:25 -0300

    finalizacao da pagina de detalhes

--- pokedex/src/pages/detailsPage/styled.js
+++ pokedex/src/pages/detailsPage/styled.js
@@ -1,16 +1,5 @@
 import styled from "styled-components";
 
-const ContainerConteudo = styled.div`
-  max-width: 100%;
-  width: 1120px;
-  margin: 0 auto;
-
-  h1{
-    margin: 60px 0 20px 0;
-    color: white;
-  }
-`
-
 const HeaderContainer = styled.div`
   display: flex;
   background-color: #0a0a0a;
@@ -31,9 +20,97 @@ const DivContainer = styled.div`
   align-items: center;
 `
 
+const ContainerConteudo = styled.div`
+  max-width: 100%;
+  width: 1160px;
+  margin: 0 auto;
+`
+
+const DivTitulo = styled.div`
+  margin: 60px 0 40px 0;
+  text-align: center;
+
+  h1{
+    display: inline-block;
+    font-size: 2em;
+    font-weight: bold;
+    color: white;
+    text-transform: capitalize;
+  }
+
+  span{
+    color: white;
+    font-size: 2em;
+    font-weight: bold;
+    margin-left: 20px;
+  }
+`
+const DivPrincipal = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
+const DivImg = styled.div`
+  max-width: 100%;
+  width: 350px;
+  margin: 0 80px;
+
+  img{
+    max-width: 100%;
+  }
+`
+
+const DivTipo = styled.div`
+  display: flex;
+  justify-content: center;
+  /* border: 1px solid red; */
+`
+
+const DivInfo = styled.div`
+  padding: 30px;
+  max-width: 100%;
+  width: 350px;
+  margin: 0 80px;
+  background-color: #E7262D;
+  border-radius: 7px;
+  box-shadow: 0px 5px 12px 5px rgba(0, 0, 0, 0.4);
+
+  h2{
+    margin-bottom: 5px;
+    text-align: center;
+  }
+
+  h4{
+    text-transform: capitalize;
+    padding: 2px 8px;
+    background-color: #DCDFE4;
+    margin-bottom: 5px;
+    border-radius: 50px;
+  }
+`
+
+const Estatisticas =  styled.div`
+  margin-bottom: 20px;
+  span{
+    font-weight: normal;
+  }
+`
+
+const Golpes = styled.div`
+
+`
+
 export{
-    ContainerConteudo,
-    HeaderContainer,
-    LogoPokemon,
-    DivContainer
+  ContainerConteudo,
+  HeaderContainer,
+  LogoPokemon,
+  DivContainer,
+  DivTitulo,
+  DivPrincipal,
+  DivImg,
+  DivInfo,
+  Estatisticas,
+  Golpes,
+  DivTipo
 }
